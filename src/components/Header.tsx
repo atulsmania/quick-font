31fae38271d180a55468de267491fb01e59d5e13
atulsmania <atul14ac@gmail.com>
2024-06-30 12:14:08 +0530

    fix: title name change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className="flex justify-between px-2 py-4 bg-gray-800">
-            <h1 className="text-lg font-bold text-white">Fontify</h1>
+            <h1 className="text-lg font-bold text-white">Quick Font</h1>
             <button className="px-2 text-white rounded bg-slate-600">
                 Contribute
             </button>
